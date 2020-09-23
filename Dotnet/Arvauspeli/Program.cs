cb66e43ebad2995058443a2aacbe0427db9f6bad
Minna Iikkanen <minna.iikkanen@gmail.com>
2020-09-23 14:54:59 +0300

    Testi

---
+++ Dotnet/Arvauspeli/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Arvauspeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tervetuloa pelaamaan Arvaa luku-peliä!");
+
+            // arvotaan satunnainen luku, joka on oikea vastaus
+            Random rand = new Random();
+            int luku = rand.Next(1,21);
+            Console.WriteLine(luku);
+           
+    
+
+        Console.WriteLine("Peli on päättynyt.");
+
+        }
+    }
+}
